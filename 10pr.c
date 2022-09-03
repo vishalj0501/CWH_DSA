39b1f7448df081c89ef0bb2e5a8f37fce59d7881
Vishal Jamboju <jvishal0501@gmail.com>
2022-09-03 13:31:06 +0400

    Add codes

---
+++ 10pr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void display(int arr[], int n){
+
+for(int i=0; i<n, ; i++){
+    
+ for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+}
+
+int intInsertion (int arr[], int size, int element ,int capacity,int index  ){
+
+
+if(size>=capacity){
+    return -1;
+
+}
+for(int i = size-1;)
+
+}
+
+int main(){
+    
+int arr[100]= {7, 8, 12, 27 , 88};
+int size = 5;
+int element = 45;
+int index=1;
+display(arr, size);
+
+
+    return 0;
+}
---
+++ 14pr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Node
+{
+    int data ;
+    struct Node *next;
+};
+
+void linkedListraversal(struct Node *ptr)
+
+int main(){
+    
+struct Node *head;
+
+
+    return 0;
+}
